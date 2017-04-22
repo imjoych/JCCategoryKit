54cc9b15acb1c28239b21f041f97ad79fa1dad96
Joych <imjoych@gmail.com>
2017-04-22 23:12:03 +0800

    Update comments

--- JCCategoryKit/UIImage+JCKit.h
+++ JCCategoryKit/UIImage+JCKit.h
@@ -20,10 +20,6 @@
  * 图片缩放
  */
 - (UIImage *)jc_scaleToSize:(CGSize)size;
-/**
- * 图片缩放
- * opaque 不透明度, 默认为YES.
- */
 - (UIImage *)jc_scaleToSize:(CGSize)size opaque:(BOOL)opaque;
 
 /// 图片压缩并转化成NSData
