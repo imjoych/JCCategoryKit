98688b1e9ffd0465e7980e51e1318c094d526c44
陈建军 <jianjun16@staff.weibo.com>
2018-04-09 11:26:58 +0800

    Method of jc_MD5String update

--- JCCategoryKit/NSString+JCKit.m
+++ JCCategoryKit/NSString+JCKit.m
@@ -119,10 +119,10 @@
 - (NSString *)jc_MD5String
 {
     const char *cString = [self UTF8String];
-    unsigned char result[16];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
     CC_MD5(cString, (CC_LONG)strlen(cString), result);
     return [NSString stringWithFormat:
-            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             result[0], result[1], result[2], result[3],
             result[4], result[5], result[6], result[7],
             result[8], result[9], result[10], result[11],
